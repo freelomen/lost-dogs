79a1a6e274b49fcc94479e3c4de963d298dd6d0a
Anton Fedorov <gtashnik2000@gmail.com>
2022-11-01 15:08:01 +0500

    Add profile action menu

--- app/src/main/java/com/example/lostdog/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/lostdog/ui/fragments/ProfileFragment.kt
@@ -1,12 +1,18 @@
 package com.example.lostdog.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.lostdog.R
 
 class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.profile_action_menu, menu)
     }
 
 }
